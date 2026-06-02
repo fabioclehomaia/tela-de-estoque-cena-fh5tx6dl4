1917e3fc24d045c87f6188d556bb3e7e0ee01ec5
Skip <skip@usecurling.com>
2026-06-01 23:38:27 -0300

    feat: Implement inventory count report screen

--- src/services/users.ts
+++ src/services/users.ts
@@ -10,3 +10,7 @@ export interface User {
 }
 
 export const getUsers = () => pb.collection('users').getFullList<User>({ sort: 'name' })
+export const createUser = (data: Partial<User> & { password?: string; passwordConfirm?: string }) =>
+  pb.collection('users').create<User>(data)
+export const updateUser = (id: string, data: Partial<User>) =>
+  pb.collection('users').update<User>(id, data)
